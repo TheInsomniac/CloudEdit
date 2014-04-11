05fe958490fff28e777800bb6914f5489d6e80a9
TheInsomniac <manxam2k@hotmail.com>
2014-04-10 19:38:10 -0600

    Reenabled auto parsing of HTML/CSS instead of hitting "Run" button

--- js/cloudEdit.js
+++ js/cloudEdit.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+(function() {
   "use strict";
   // Globals
   // ---
@@ -12,6 +12,100 @@ $(document).ready(function() {
     Bootstrap: false,
     Foundation: false
   };
+
+  // ---
+  // End Globals
+
+  // Create Text Area panes
+  // Init ACE Editor and set options;
+  (function initAce() {
+    var aceTheme;
+    if (localStorage.getItem("theme")) {
+      aceTheme = localStorage.getItem("theme");
+    } else {
+      aceTheme = "ace/theme/chrome";
+    }
+
+    // HTML Editor
+    window.htmlField = ace.edit("html");
+    htmlField.session.setUseWorker(false);
+    htmlField.setTheme(aceTheme);
+    htmlField.setDisplayIndentGuides(true);
+    htmlField.getSession().setMode("ace/mode/html");
+    htmlField.getSession().setTabSize(2);
+    htmlField.getSession().setUseSoftTabs(true);
+
+    // CSS Editor
+    window.cssField = ace.edit("css");
+    cssField.setTheme(aceTheme);
+    cssField.setDisplayIndentGuides(true);
+    cssField.getSession().setMode("ace/mode/css");
+    cssField.getSession().setTabSize(2);
+    cssField.getSession().setUseSoftTabs(true);
+
+    // JS Editor
+    window.jsField = ace.edit("js");
+    jsField.setTheme(aceTheme);
+    jsField.setDisplayIndentGuides(true);
+    jsField.getSession().setMode("ace/mode/javascript");
+    jsField.getSession().setTabSize(2);
+    jsField.getSession().setUseSoftTabs(true);
+
+    // Console
+    window.consoleField = ace.edit("console");
+    consoleField.session.setUseWorker(false);
+    consoleField.setTheme(aceTheme);
+    consoleField.getSession().setMode("ace/mode/javascript");
+    consoleField.setReadOnly(true);
+    consoleField.setHighlightActiveLine(false);
+    consoleField.setHighlightGutterLine(false);
+    consoleField.renderer.$cursorLayer.element.style.opacity=0;
+    consoleField.container.style.pointerEvents="none";
+    consoleField.setShowPrintMargin(false);
+    consoleField.getSession().selection.on("changeSelection", function() {
+      consoleField.clearSelection();
+    });
+
+    // Store data in sessionStorage to prevent data loss on window reload
+    // and run preview function to emit changes to iframe
+    (function sessionStorageSet() {
+      htmlField.getSession().on("change", function(e) {
+        sessionStorage.setItem("html", htmlField.getValue());
+        preview();
+      });
+      cssField.getSession().on("change", function(e) {
+        sessionStorage.setItem("css", cssField.getValue());
+        preview(2000);
+      });
+      jsField.getSession().on("change", function(e) {
+        sessionStorage.setItem("js", jsField.getValue());
+      });
+    })();
+
+    // Retrieve values from sessionStorage if set
+    (function sessionStorageGet() {
+      if (sessionStorage.getItem("html")) {
+        htmlField.setValue(sessionStorage.getItem("html"));
+        htmlField.clearSelection();
+      } else {
+        htmlField.setValue("<!-- Do not place html/head/body tags here.\n" +
+          "Insert the tags as would normally be used in your\n" +
+          "body element. <script> tags ARE allowed, though\n" +
+          "they're best placed at the end of your HTML -->\n");
+        htmlField.clearSelection();
+      }
+      if (sessionStorage.getItem("css")) {
+        cssField.setValue(sessionStorage.getItem("css"));
+        cssField.clearSelection();
+      }
+      if (sessionStorage.getItem("js")) {
+        jsField.setValue(sessionStorage.getItem("js"));
+        jsField.clearSelection();
+      }
+    })();
+
+  })();
+  // END ACE Editor
 
   // Toggle Text Areas from Displaying
   $("#htmlToggle").on("click", function(el) {
@@ -133,6 +227,8 @@ $(document).ready(function() {
       html += '<script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.7.1/modernizr.min.js"></script>\n';
     }
     if (use.Less) {
+      // Set LESS global variable to turn errorReporting off and mode to production
+      html += '<script>\nless={env: "production", errorReporting: null}\n</script>\n';
       html += '<script src="https://cdnjs.cloudflare.com/ajax/libs/less.js/1.7.0/less.min.js"></script>\n';
     }
     html += '</head>\n';
@@ -152,16 +248,23 @@ $(document).ready(function() {
   }
 
   // Publish output from HTML, CSS, and JS textareas in the iframe below
-  // when "Run" button clicked.
-  $("#run").on("click", function(el) {
-    el.preventDefault();
+  //$("#run").on("click", function(el) {
+  //el.preventDefault();
+  function preview(delay) {
+    delay = delay || 500;
+    var timer = null;
+    if (timer) {
+      window.clearTimeout(timer);
+    }
+    timer = window.setTimeout(function() {
+      timer = null;
+      // pass true as we want the pseudo console.js script
+      var textToWrite = buildOutput(true);
 
-    // pass true as we want the pseudo console.js script
-    var textToWrite = buildOutput(true);
-
-    (document.getElementById("iframe").contentWindow.document).write(textToWrite);
-    (document.getElementById("iframe").contentWindow.document).close();
-  });
+      (document.getElementById("iframe").contentWindow.document).write(textToWrite);
+      (document.getElementById("iframe").contentWindow.document).close();
+    }, delay);
+  }
 
   // Download HTML/CSS/JS
   // Source: http://thiscouldbebetter.wordpress.com/2012/12/18/loading-editing-and-saving-a-text-file-in-html5-using-javascrip/
@@ -497,4 +600,4 @@ $(document).ready(function() {
     return message;
   };
 
-});
+})();
